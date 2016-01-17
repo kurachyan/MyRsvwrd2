15845181073191fe3f7bb087a91d8e33e0fb8c51
kurachyan <e_tamakou@outlook.jp>
2016-01-17 21:05:02 +0900

    初期設定２

--- Rsvwrd/CS_Rsvwrd.cs
+++ Rsvwrd/CS_Rsvwrd.cs
@@ -42,6 +42,27 @@ namespace Rsvwrd
             RSV_OTHER           // 予約語６：その他
         }
         private RsvCode _rsvcode;
+        public Boolean Rsv
+        {   // 予約語有無確認　[false]:予約語なし [true]:予約語あり
+            get
+            {
+                if (_rsvcode == RsvCode.RSV_NONE)
+                {   // 予約語未検出？
+                    return (false);
+                }
+                else
+                {   // 予約語検出
+                    return (true);
+                }
+            }
+        }
+        public int RsvKind
+        {
+            get
+            {
+                return ((int)_rsvcode);
+            }
+        }
 
         // 予約語１：クラス
         private string[] _RsvTable1 =
@@ -167,7 +188,7 @@ namespace Rsvwrd
             _rsvcode = RsvCode.RSV_NONE;    // 予約語：未定義
         }
         public void Exec()
-        {   // 右側余白情報を削除（固定区切り）
+        {   // 予約語確認
             if (!_empty)
             {   // バッファーに実装有り
                 _wbuf = _wbuf.Trim(_trim);       // 右側余白情報を削除
@@ -177,13 +198,75 @@ namespace Rsvwrd
                     Clear();           // 作業領域の初期化
                 }
                 else
-                {
-                    // 予約語検索・評価
+                {   // 予約語検索・評価
+                    foreach (var __rsv in _RsvTable1)
+                    {   // クラス関連の予約語を評価する
+                        if (__rsv == _wbuf)
+                        {   // 予約語定義が有るか？
+                            _rsvcode = RsvCode.RSV_CLASS;    // 予約語：クラス
+                            break;
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable2)
+                        {   // 例外関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_EXCEPT;    // 予約語：例外
+                               break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable3)
+                        {   // アクセス修飾子関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_ACCESS;    // 予約語：アクセス修飾子
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable4)
+                        {   // 制御フロー関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_CONTROL;    // 予約語：制御フロー
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable5)
+                        {   // データ型関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_DATA;    // 予約語：データ型
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable6)
+                        {   // その他関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_OTHER;    // 予約語：その他
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
         public void Exec(String msg)
-        {   // 右側余白情報を削除（固定区切り）
+        {   // 予約語確認
             Setbuf(msg);                 // 入力内容の作業領域設定
 
             if (!_empty)
@@ -195,8 +278,70 @@ namespace Rsvwrd
                     Clear();           // 作業領域の初期化
                 }
                 else
-                {
-                    // 予約語検索・評価
+                {   // 予約語検索・評価
+                    foreach (var __rsv in _RsvTable1)
+                    {   // クラス関連の予約語を評価する
+                        if (__rsv == _wbuf)
+                        {   // 予約語定義が有るか？
+                            _rsvcode = RsvCode.RSV_CLASS;    // 予約語：クラス
+                            break;
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable2)
+                        {   // 例外関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_EXCEPT;    // 予約語：例外
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable3)
+                        {   // アクセス修飾子関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_ACCESS;    // 予約語：アクセス修飾子
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable4)
+                        {   // 制御フロー関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_CONTROL;    // 予約語：制御フロー
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable5)
+                        {   // データ型関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_DATA;    // 予約語：データ型
+                                break;
+                            }
+                        }
+                    }
+                    if (_rsvcode == RsvCode.RSV_NONE)
+                    {   // 予約語は未定義？
+                        foreach (var __rsv in _RsvTable6)
+                        {   // その他関連の予約語を評価する
+                            if (__rsv == _wbuf)
+                            {   // 予約語定義が有るか？
+                                _rsvcode = RsvCode.RSV_OTHER;    // 予約語：その他
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
